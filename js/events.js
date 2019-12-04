d3f056454ef9e1571dab607fe1121e81c2099b2e
ralph <ralphadp@gmail.com>
2019-12-04 18:28:15 -0400

    set responsive menu

--- js/events.js
+++ js/events.js
@@ -1,8 +1,10 @@
-(function() {
+(() => 
+{
     document.getElementById('input-search-art').addEventListener('keypress', (event) => {
         if (event.keyCode === 13) {
             alert('SEARCHING ...');
         }
     });
 
-})();
+}
+)();
