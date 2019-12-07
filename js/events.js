df201bf9f411ac2e6f368517cb7927d1c65fcc10
ralph <ralphadp@gmail.com>
2019-12-07 01:20:10 -0400

    Create Object to handle the dialogs frames

--- js/events.js
+++ js/events.js
@@ -8,78 +8,10 @@
 
     /***********************************************************************/
 
-    const stopTouchMoveEvent = (event) => {
-        event.preventDefault();
-        event.returnValue = false;
-    };
-
-    const enableScrolling = () => {
-        document.body.style.height = null;
-        document.body.style.overflow = null;
-        document.body.removeEventListener('touchmove', stopTouchMoveEvent);
-    }
-
-    const disableScrolling = () => {
-        document.body.style.height = "100%";
-        document.body.style.overflow = "hidden";
-        document.body.addEventListener('touchmove', stopTouchMoveEvent);
-    }
-
-    const showPiece = (IDframe) => {
-        document.getElementById(IDframe).style.display = "block";
-    }
-
-    const hidePiece = (IDframe) => {
-        document.getElementById(IDframe).style.display = "none";
-    }
-
-    const openFrameEvent = () => {
-        showPiece("piece");
-        disableScrolling();
-    };
-
-    const closeFrameEvent = () => {
-        hidePiece("piece");
-        enableScrolling();
-    };
-
-    const openPhotoEvent = (event) => {
-        ///temporary line
-        document.getElementById("photo-image").src = event.srcElement.currentSrc;
-
-        /****************/ 
-        showPiece("piece-maximized");
-        disableScrolling();
-    };
-
-    const closePhotoEvent = () => {
-        hidePiece("piece-maximized");
-        enableScrolling();
-    };
-
-    const openListEvent = (event) => {
-        showPiece("list");
-        disableScrolling();
-    };
-
-    const closeListEvent = () => {
-        hidePiece("list");
-        enableScrolling();
-    };
-
-    const openPickingEvent = (event) => {
-        showPiece("cart-picking");
-        disableScrolling();
-    };
-
-    const closePickingEvent = () => {
-        hidePiece("cart-picking");
-        enableScrolling();
-    };
-
-    const stopEventsChain = (event) => {
-        event.stopPropagation();
-    }
+    const pieceFrame = new Frame('piece-form');
+    const piecePhoto = new Photo('piece-maximized');
+    const availableFrame = new Frame('list');
+    const pickedFrame = new Frame('cart-picking');
 
     /***********************************************************************************************/
 
@@ -87,49 +19,24 @@
 
     const infoButton = document.getElementsByClassName("piece-info-button");
     for (let index = 0; index < infoButton.length; index++) {
-        infoButton.item(index).addEventListener('click', openFrameEvent);    
+        infoButton.item(index).addEventListener('click', () => pieceFrame.show());
     }
-
-    document.getElementById("piece").addEventListener('click', closeFrameEvent);//fog
-
-    document.getElementById("box-close").addEventListener('click', closeFrameEvent);//close-button
-
-    document.getElementById("info-frame").addEventListener('click', stopEventsChain);
 
     /*PHOTO PIECE */
 
     const thumbList = document.getElementsByClassName("thumb-pic");
     for (let index = 0; index < thumbList.length; index++) {
-        thumbList.item(index).addEventListener('click', openPhotoEvent);    
+        thumbList.item(index).addEventListener('click', (event) => piecePhoto.show(event));
     }
-
-    document.getElementById("piece-maximized").addEventListener('click', closePhotoEvent);//fog
-    
-    document.getElementById("photo").addEventListener('click', stopEventsChain);
 
     /*LIST PIECE */
 
-    document.getElementById("open-list").addEventListener('click', openListEvent);
-
-    document.getElementById("open-list-footer").addEventListener('click', openListEvent);
-
-    document.getElementById("list").addEventListener('click', closeListEvent);//fog
-
-    document.getElementById("list-box-close").addEventListener('click', closeListEvent);//close-button
-    
-    document.getElementById("list-frame").addEventListener('click', stopEventsChain);
-
+    document.getElementById("open-list").addEventListener('click', () => availableFrame.show());
+    document.getElementById("open-list-footer").addEventListener('click', () => availableFrame.show());
+ 
     /*PICkED PIECES */
 
-    document.getElementById("open-picked").addEventListener('click', openPickingEvent);
-
-    document.getElementById("open-picked-footer").addEventListener('click', openPickingEvent);
-
-    document.getElementById("cart-picking").addEventListener('click', closePickingEvent);//fog
-
-    document.getElementById("pick-box-close").addEventListener('click', closePickingEvent);//close-button
-    
-    document.getElementById("pick-frame").addEventListener('click', stopEventsChain);
-
+    document.getElementById("open-picked").addEventListener('click', () => pickedFrame.show());
+    document.getElementById("open-picked-footer").addEventListener('click', () => pickedFrame.show());
 
 })();
