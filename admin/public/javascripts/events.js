f8122d8f355c4f1d9e4ca8ab56d40e66c489963d
ralph <ralphadp@gmail.com>
2019-12-18 21:32:37 -0400

    Linking menu toogling

--- admin/public/javascripts/events.js
+++ admin/public/javascripts/events.js
@@ -1,20 +1,41 @@
 (() => {
 
-    let toogleMenu = (menuItem) => {
+    /**
+     * Toogle aside menu 
+     * @param {*} menuItem 
+     * @param {*} affectedMenuItem 
+     * @param {*} target 
+     */
+    const toogleMenu = (menuItem, affectedMenuItem, target) => {
         menuItem.addEventListener('click', () => {
+            const EMPTY = '';
+            const DOT = ' •';
             let status = window.getComputedStyle(menuItem, ':after').getPropertyValue('content');
-    
-            status = status.replace(/"/g, '');
+           
+            status = status.replace(/"/g, EMPTY);
             if (status.length) {
-                menuItem.setAttribute('status', '');
+                menuItem.setAttribute('status', EMPTY);
+                affectedMenuItem.setAttribute('status', EMPTY);
+                target.style.display = 'none';
             } else {
-                menuItem.setAttribute('status', ' •');
+                menuItem.setAttribute('status', DOT);
+                affectedMenuItem.setAttribute('status', DOT);
+                target.style.display = 'block';
             }
         });
     };
 
-    toogleMenu(document.getElementsByClassName('item').item(1));
-    toogleMenu(document.getElementsByClassName('vertical-item').item(1));
+    toogleMenu(
+        document.getElementsByClassName('item').item(1),
+        document.getElementsByClassName('vertical-item').item(1),
+        document.getElementsByTagName('aside').item(0)
+    );
+
+    toogleMenu(
+        document.getElementsByClassName('vertical-item').item(1),
+        document.getElementsByClassName('vertical-item').item(1),
+        document.getElementsByTagName('aside').item(0)  
+    );
     
 
 })();
