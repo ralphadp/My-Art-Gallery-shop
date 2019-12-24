744267712257ea17f7a8741017ffc78506fc366f
ralph <ralphadp@gmail.com>
2019-12-24 19:39:31 -0400

    Fetch messages from https://mailtrap.io feature

--- admin/routes/index.js
+++ admin/routes/index.js
@@ -53,7 +53,94 @@ router.get('/profile', function(req, res, next) {
 
 /* GET messages page. */
 router.get('/messages', function(req, res, next) {
-  res.render('messages', { title: 'Inbox messages' });
+
+  /*TODO: need to hide the mailtrap.io token and set it from configuration*/
+  let headers = {
+    "Api-Token": "c91bb9378f5f7860b45ae4a1f0b29fab",
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+  };
+
+  let txtHeaders = {
+    "Api-Token": "c91bb9378f5f7860b45ae4a1f0b29fab",
+    "Accept": "text/plain",
+  };
+
+  fetch(
+    'https://mailtrap.io/api/v1/inboxes',
+    {
+      method: 'GET', 
+      headers: headers
+    }
+  ).then(response => response.json())
+   .then(content => {
+
+        if (typeof content.error !== 'undefined') {
+
+            res.render('messages', { title: 'Inbox messages', error: content.error });
+
+            return;
+        }
+
+        /* Taking the first inbox only, no several vendors */
+        const inboxData = {
+          id: content[0].id,
+          name: content[0].name,
+          count: content[0].emails_count
+        }
+
+        /*Get all the messages from the inbox */
+        fetch(
+          `https://mailtrap.io/api/v1/inboxes/${inboxData.id}/messages`,
+          {
+            method: 'GET', 
+            headers: headers
+          }
+        ).then(response => response.json())
+         .then(content => {
+           
+            let emailContents = [];
+            let textRequests = [];
+
+            content.forEach(element => {
+
+                let item = {
+                    id : element.id,
+                    subject: element.subject,
+                    from: element.from_email,
+                    sentAt: element.sent_at,
+                    urlTextComment: 'https://mailtrap.io' + element.txt_path
+                };
+
+                textRequests.push(
+                    fetch(item.urlTextComment, {method: 'GET', headers: txtHeaders}).then(response => response.text())
+                );
+
+                emailContents.push(item);
+            });
+
+            /**Get all the messages bodys */
+             Promise.all(textRequests)
+            .then(texts => {
+
+              texts.forEach((text, index) => {
+                    emailContents[index].text = text;
+                });
+            })
+            .catch (error => console.log(error))
+            .finally(() => {
+ 
+                res.render('messages', { title: 'Inbox messages', box: inboxData, messages: emailContents, error: null });
+            });
+
+         })
+         .catch (error => console.log(error));
+    })
+    .catch (error => {
+      console.log(error);
+
+      res.render('messages', { title: 'Inbox messages', error: error });
+    });
 });
 
 /* GET configuration page. */
