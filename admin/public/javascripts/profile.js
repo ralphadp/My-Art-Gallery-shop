b8a55c6c17d10c97e8dea01b7809310cbf57fa42
ralph <ralphadp@gmail.com>
2019-12-27 21:35:36 -0400

    Add profile photo upload

--- admin/public/javascripts/profile.js
+++ admin/public/javascripts/profile.js
@@ -17,3 +17,26 @@
     });
 
 })();
+
+/**
+ * In case image upload was good, set the user photo
+ * @param {*} response 
+ */
+const uploadSuccess = (response) => {
+    let newExternalId = 'ERROR, no file name code was back from server.';
+    if (response.files.length) {
+        newExternalId = response.files[0].split('.')[0];    
+    }
+    document.getElementById('profile-photo-id').value = newExternalId;
+    document.getElementById('profile-image-stored').src = `http://localhost:8888/api/image/${response.type}/${newExternalId}`;
+
+    alert(response.message);
+};
+
+/**
+ * In case the failure, show the error
+ * @param {*} responseError 
+ */
+const uploadFail = (responseError) => {
+    alert(responseError.error);
+};
