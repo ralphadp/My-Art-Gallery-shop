81a7219bcbf57cb8c9e0d32c3e4af6d1e5dbd767
ralph <ralphadp@gmail.com>
2020-01-09 02:49:15 -0400

    clean code

--- admin/public/javascripts/profile.js
+++ admin/public/javascripts/profile.js
@@ -8,7 +8,7 @@
         const profile = document.getElementsByClassName('profile_input');
         for (let index = 0; index < profile.length; index++) {
             profile.item(index).readOnly = !enable;
-            profile.item(index).disabled = !enable;    
+            profile.item(index).disabled = !enable;
         }
     }
 
