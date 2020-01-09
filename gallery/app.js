a13819ef9e32d98e7223ea976988a9b5ca868092
ralph <ralphadp@gmail.com>
2020-01-09 03:28:58 -0400

    Fix error / 404 pages

--- gallery/app.js
+++ gallery/app.js
@@ -56,7 +56,7 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.title = 'Error';
+  res.locals.title = (err.status === 404) ? 'Page Not found' : 'Error';
   res.locals.currentUser = req.session;
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
