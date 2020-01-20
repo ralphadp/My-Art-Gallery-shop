9bc58fdfe605bc2338ba5b8a92d3b6f5db1b9350
ralph <ralphadp@gmail.com>
2020-01-20 17:05:16 -0400

    - Add namespace 'art' - Change gallery-pictures:v1 to gallery-images:v1 image name - Change Jwt deploy and service configuration

--- build-local.sh
+++ build-local.sh
@@ -6,17 +6,21 @@ eval $(minikube docker-env)
 
 echo "Start building images"
 cd ./ArtGaleryShopping/
+
 cd sqlDump/
 docker build -t gallery-mysql:v1 .
 cd ..
+
 cd gscripts/
 docker build -t gallery-scripts:v1 .
 cd ..
+
 cd galleryJWT/
 docker build -t gallery-jwt:v1 .
 cd ..
+
 cd galleryPicturesProvider/
-docker build -t gallery-pictures:v1 .
+docker build -t gallery-images:v1 .
 cd ..
 
 echo "Images done..."
