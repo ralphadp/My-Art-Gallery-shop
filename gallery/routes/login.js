71157f4a9a1fb81fed36ac1686ec33c178d70b6c
ralph <ralphadp@gmail.com>
2020-01-21 19:39:52 -0400

    Checking login errors

--- gallery/routes/login.js
+++ gallery/routes/login.js
@@ -22,101 +22,105 @@ router.post('/validation', validate('login'), function(req, res, next) {
 
     let userInfo = {};
     let response = {};
+    try {
+        const errors = validateResult(req);
 
-    const errors = validateResult(req);
 
-    if (!errors.isEmpty()) {
+        if (!errors.isEmpty()) {
 
-        response = {
-            result: false,
-            message: errors.array().join('\n')
-        };
-        res.send(response);
-        return;
-    }
+            response = {
+                result: false,
+                message: errors.array().join('\n')
+            };
+            res.send(response);
+            return;
+        }
 
-    const user = new users();
+        const user = new users();
 
-    user.verify(req.body.username, req.body.password).then(result => {
-        if (result.length) {
-            const validUser = result[0];
-            if (validUser.active === 1) {
+        user.verify(req.body.username, req.body.password).then(result => {
+            if (result.length) {
+                const validUser = result[0];
+                if (validUser.active === 1) {
 
-                userInfo.username = validUser.first_name + ' ' + validUser.last_name;
-                userInfo.userEmail = validUser.email;
-                userInfo.userId = validUser.external_id;
-                userInfo.photo = validUser.photo;
+                    userInfo.username = validUser.first_name + ' ' + validUser.last_name;
+                    userInfo.userEmail = validUser.email;
+                    userInfo.userId = validUser.external_id;
+                    userInfo.photo = validUser.photo;
 
+                    response = {
+                        result: true,
+                        data: validUser,
+                        message: 'Successful Autentication'
+                    };
+                } else {
+                    response = {
+                        result: false,
+                        data: validUser,
+                        message: 'User not active yet.'
+                    };
+                }
+            } else {
                 response = {
-                    result: true,
-                    data: validUser,
-                    message: 'Successful Autentication'
+                    result: false,
+                    data: null,
+                    message: 'Wrong username or password.'
+                };
+            }
+        }).catch(error => {
+            if (typeof error.sqlMessage !== 'undefined') {
+                response = {
+                    result: false,
+                    data: null,
+                    message: error.sqlMessage
                 };
             } else {
                 response = {
                     result: false,
-                    data: validUser,
-                    message: 'User not active yet.'
+                    data: null,
+                    message: error
                 };
             }
-        } else {
-            response = {
-                result: false,
-                data: null,
-                message: 'Wrong username or password.'
-            };
-        }
-    }).catch(error => {
-        if (typeof error.sqlMessage !== 'undefined') {
-            response = {
-                result: false,
-                data: null,
-                message: error.sqlMessage
-            };
-        } else {
-            response = {
-                result: false,
-                data: null,
-                message: error
-            };
-        }
-    }).finally(() => {
-        if (response.result) {
+        }).finally(() => {
+            if (response.result) {
 
-            userInfo.hours = '1h';
+                userInfo.hours = '1h';
 
-            fetch(
-                `${service.jwtHost}/api/generate-by-info/`,
-                {
-                    method: 'POST',
-                    headers:{
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(userInfo)
-                }
-            )
-            .then(jwtResponse => jwtResponse.json())
-            .then(jwtResponse => {
-                if (jwtResponse.success) {
-                    const jwtCookie = `access_token=${jwtResponse.token}; Path=/; Max-Age=604800; HttpOnly;`;
-                    res.header("Set-Cookie", jwtCookie);
-                } 
-                console.log('jwt generate response:', jwtResponse.message);
-            }).catch(error => {
-                console.log(error);
-                response = {
-                    result: false,
-                    data: null,
-                    message: 'Could not get authorization from remote server, try later' 
-                };
-            }).finally(()=> {
+                fetch(
+                    `${service.jwtHost}/api/generate-by-info/`,
+                    {
+                        method: 'POST',
+                        headers:{
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(userInfo)
+                    }
+                )
+                .then(jwtResponse => jwtResponse.json())
+                .then(jwtResponse => {
+                    if (jwtResponse.success) {
+                        const jwtCookie = `access_token=${jwtResponse.token}; Path=/; Max-Age=604800; HttpOnly;`;
+                        res.header("Set-Cookie", jwtCookie);
+                    } 
+                    console.log('jwt generate response:', jwtResponse.message);
+                }).catch(error => {
+                    console.log(error);
+                    response = {
+                        result: false,
+                        data: null,
+                        message: 'Could not get authorization from remote server, try later' 
+                    };
+                }).finally(()=> {
+                    res.send(response);
+                });
+            } else {
                 res.send(response);
-            });
-        } else {
-            res.send(response);
-        }
-    });
-    
+            }
+        });
+    } catch(error) {
+        console.log(error);
+        throw error;
+    }
 });
 
 /* POST forgoteen password resend route. */
