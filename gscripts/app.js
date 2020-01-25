ddc9372599e6ebbe9509d6c82f57a2495f548f63
ralph <ralphadp@gmail.com>
2020-01-25 18:01:42 -0400

    Display version at root path for gscripts

--- gscripts/app.js
+++ gscripts/app.js
@@ -2,15 +2,18 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var pjson = require('./package.json');
 
 var app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  res.send(`Gallery Scripts v.${pjson.version}`);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
