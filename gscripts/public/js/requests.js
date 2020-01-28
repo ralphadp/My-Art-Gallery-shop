63961af0f5751d37e2e08807eecc272f4a0d0c52
ralph <ralphadp@gmail.com>
2020-01-28 16:40:32 -0400

    Improvements in request function

--- gscripts/public/js/requests.js
+++ gscripts/public/js/requests.js
@@ -1,9 +1,41 @@
 let request = config => {
+
+    /**
+     * Set message in case to get undeterminated wrong response from server
+     * @param {*} paramMessage
+     */
+    function badResponse(paramMessage) {
+        return JSON.stringify({
+            result: false,
+            success: false,
+            message: paramMessage
+        });
+    }
+
     return new Promise((resolve, reject) => {
 
         let xhr = new XMLHttpRequest();
 
         xhr.open(config.method || "GET", config.url);
+
+        let parseRemoteResponse = () => {
+            let response = null;
+
+            if (xhr.status) {
+                response = xhr.responseText ? xhr.responseText : xhr.response;
+                response = response ? response : badResponse(xhr.statusText);
+            } else {
+                response = badResponse('Remote server unreachable');
+            }
+
+            if (config.result === 'object') {
+                response = JSON.parse(response);
+            }
+
+            console.log(response);
+
+            return response;
+        };
 
         if (config.headers) {
             //for application/x-www-form-urlencoded, multipart/form-data, json, JWT
@@ -14,13 +46,15 @@ let request = config => {
 
         xhr.onload = () => {
             if (xhr.status >= 200 && xhr.status < 300) {
-                resolve(xhr.response);
+                resolve(parseRemoteResponse());
             } else {
-                reject(xhr.statusText);
+                reject(parseRemoteResponse());
             }
         };
 
-        xhr.onerror = () => reject(xhr.statusText);
+        xhr.onerror = () => {
+            reject(parseRemoteResponse());
+        }
 
         xhr.send(config.body);
     });
